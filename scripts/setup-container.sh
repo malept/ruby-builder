ded5f691833795c1e01857ed0c8b06695f001079
Mark Lee <asdf@lazymalevolence.com>
2025-01-25 20:26:05 -0800

    Update apt lists

--- scripts/setup-container.sh
+++ scripts/setup-container.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 distro="$1"
 
 distro_name="$(cut -d: -f1 <<<"$distro")"
@@ -7,6 +9,7 @@ distro_name="$(cut -d: -f1 <<<"$distro")"
 
 case "$distro_name" in
 ubuntu)
+  apt-get update
   apt-get install -y git
   ;;
 
