dba9392a8495ea527eccda3ea71e861443c4c956
Mark Lee <asdf@lazymalevolence.com>
2025-01-25 20:29:44 -0800

    Install dev packages

--- scripts/setup-container.sh
+++ scripts/setup-container.sh
@@ -10,7 +10,7 @@ distro_name="$(cut -d: -f1 <<<"$distro")"
 case "$distro_name" in
 ubuntu)
   apt-get update
-  apt-get install -y git curl
+  apt-get install -y git curl autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
   ;;
 
 esac
