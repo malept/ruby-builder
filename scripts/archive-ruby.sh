de4d6e8114a33347d0b529127ef31289af81c784
Mark Lee <asdf@lazymalevolence.com>
2025-01-26 23:29:01 -0800

    Use tar --directory

--- scripts/archive-ruby.sh
+++ scripts/archive-ruby.sh
@@ -7,6 +7,4 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ruby_version="$1"
 distro="$("$DIR"/normalize_distro.sh "$2")"
 
-pushd "$HOME/.asdf/installs/ruby"
-tar --strip-components=1 -cf "ruby-${ruby_version}_${distro}.tar.xz" "$ruby_version"
-popd
+tar --directory="$HOME/.asdf/installs/ruby/$ruby_version" --create --file "$DIR/ruby-${ruby_version}_${distro}.tar.xz" .
