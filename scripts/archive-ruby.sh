a8d2e7052140a63234d3e30f8d1ab1d3a4de7b34
Mark Lee <asdf@lazymalevolence.com>
2025-01-26 23:36:38 -0800

    Put tarball in the correct location

--- scripts/archive-ruby.sh
+++ scripts/archive-ruby.sh
@@ -7,4 +7,4 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ruby_version="$1"
 distro="$("$DIR"/normalize_distro.sh "$2")"
 
-tar --directory="$HOME/.asdf/installs/ruby/$ruby_version" --create --file "$DIR/ruby-${ruby_version}_${distro}.tar.xz" .
+tar --directory="$HOME/.asdf/installs/ruby/$ruby_version" --create --file "$DIR/../ruby-${ruby_version}_${distro}.tar.xz" .
