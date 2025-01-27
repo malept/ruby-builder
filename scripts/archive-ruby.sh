eecab2d51d29484538eca62a4c9a591ce3ac9060
Mark Lee <asdf@lazymalevolence.com>
2025-01-26 23:20:19 -0800

    Fix path prefix in tar file

--- scripts/archive-ruby.sh
+++ scripts/archive-ruby.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 ruby_version="$1"
 distro="$("$DIR"/normalize_distro.sh "$2")"
 
-tar -cf "ruby-${ruby_version}_${distro}.tar.xz" "$HOME/.asdf/installs/ruby/$ruby_version"
+pushd "$HOME/.asdf/installs/ruby"
+tar --strip-components=1 -cf "ruby-${ruby_version}_${distro}.tar.xz" "$ruby_version"
+popd
