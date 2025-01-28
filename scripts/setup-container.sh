e7f6a6cff85e0affc382476881eedda30d13478d
Mark Lee <asdf@lazymalevolence.com>
2025-01-27 23:08:14 -0800

    Add lint/format tasks & lint in CI

--- scripts/setup-container.sh
+++ scripts/setup-container.sh
@@ -9,6 +9,8 @@ major_version="$(echo "$version" | cut -d. -f1)"
 minor_version="$(echo "$version" | cut -d. -f2)"
 
 enable_yjit() {
+  # shellcheck disable=SC2235
+  # Why: don't care about subshell overhead in this case.
   if test "$major_version" -gt 3 || (test "$major_version" -eq 3 && test "$minor_version" -ge 2); then
     echo 0
   else
