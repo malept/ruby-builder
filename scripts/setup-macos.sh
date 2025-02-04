8c44b821b947866c4767d271b3a6bc97b72546e9
Mark Lee <malept@users.noreply.github.com>
2025-02-03 23:39:06 -0800

    feat: add macOS support (#4)

---
+++ scripts/setup-macos.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -e
+
+DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
+version="$1"
+
+extra_brew_pkgs=""
+if "$DIR"/enable_yjit.sh "$version"; then
+  extra_brew_pkgs="$extra_brew_pkgs rust"
+  echo "yjit_arg=--enable-yjit" >>"$GITHUB_OUTPUT"
+fi
+
+# List from: https://github.com/rbenv/ruby-build/wiki#macos
+# shellcheck disable=SC2086
+brew install openssl@3 readline libyaml gmp autoconf bash $extra_brew_pkgs
