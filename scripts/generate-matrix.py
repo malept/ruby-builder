d7074b2f3d1abe494ad63d13f9671a941e28622d
Mark Lee <asdf@lazymalevolence.com>
2025-08-07 00:46:49 -0700

    chore: remove constraint from .ruby-version files

--- scripts/generate-matrix.py
+++ scripts/generate-matrix.py
@@ -8,7 +8,7 @@ root_dir = (Path(__file__) / ".." / "..").resolve(strict=True)
 with open(os.environ["GITHUB_OUTPUT"], "a") as output:
     output.write("ruby_versions=")
     versions = [
-        p.read_text().strip().split()[1]
+        p.read_text().strip()
         for p in sorted((root_dir / "versions").glob("*/.ruby-version"))
     ]
     json.dump(versions, output)
