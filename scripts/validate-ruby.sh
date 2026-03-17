d222aef0dbb068b5c39ce95aff0aa6e62a60aa1d
Mark Lee <asdf@lazymalevolence.com>
2026-03-17 00:33:27 -0700

    Adjust validation script

--- scripts/validate-ruby.sh
+++ scripts/validate-ruby.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # shellcheck source=scripts/include/common.sh
 source "$DIR/include/common.sh"
 
-expected_version="$1"
+major_minor="${1:-}"
+
+cd versions/"$major_minor"
+
 actual_version="$(ruby --version | awk '{print $2}' | cut -d p -f1)"
 
-if [[ "$expected_version" != "$actual_version" ]]; then
-  fail "The actual Ruby version ($actual_version) does not match the expected version ($expected_version)"
+if ! [[ $actual_version == "$major_minor"* ]]; then
+  fail "The actual Ruby version ($actual_version) does not start with '$major_minor'"
 fi
 
-location="$(mise where ruby@"$expected_version")"
+location="$(mise where ruby@"$major_minor")"
 
 persist_value install_location "$location"
